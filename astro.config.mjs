758d93d84a1cb4a47cf179a7966eae28bb807478
Muhammad Maulana Hikam <maulanahikam085@gmail.com>
2026-06-20 09:33:47 +0700

    chore: update dependencies and enable dist tracking for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflareAdapter from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'static',
   integrations: [tailwind()],
-  adapter: cloudflareAdapter(),
   site: 'https://hiworld.tech',
   build: {
     inlineStylesheets: 'auto'
