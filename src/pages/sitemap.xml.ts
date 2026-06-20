51dfe092ecdce6f63b68934ff84f63951601bde3
Muhammad Maulana Hikam <maulanahikam085@gmail.com>
2026-06-20 09:30:47 +0700

    chore: add SEO configuration with robots.txt and sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,20 @@
+export function GET() {
+  const siteUrl = 'https://hiworld.tech';
+  const lastModified = new Date().toISOString().split('T')[0];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${siteUrl}/</loc>
+    <lastmod>${lastModified}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
